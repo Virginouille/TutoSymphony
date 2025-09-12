60ecaa1d98913d5789361feec119e7e1a44f9f7a
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-12 11:45:15 +0200

    validation des données

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -7,22 +7,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\String\Slugger\AsciiSlugger;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Sequentially;
 
 class RecipeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'empty_data' => ''
+            ])
             ->add('slug', TextType::class, [
                 'required' => false
             ])
-            ->add('content')
+            ->add('content', TextareaType::class, [
+                'empty_data' => ''
+            ])
             ->add('duration')
             ->add('save', SubmitType::class, [
                 'label' => 'Envoyer'
@@ -57,7 +65,7 @@ class RecipeType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Recipe::class,
+            'data_class' => Recipe::class
         ]);
     }
 }
