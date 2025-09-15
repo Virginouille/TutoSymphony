d8256d26570449fcb1a0935f3de4246bb0d0bc89
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-09-15 11:10:29 +0200

    finalisation tp form

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,6 +24,16 @@ class ContactType extends AbstractType
             ])
             ->add('message', TextareaType::class, [
                 'empty_data' => ''
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+            ->add('service', ChoiceType::class, [
+                'choices' => [
+                    'Compta' => 'compat@demo.fr',
+                    'Supprot' => 'support@demo.fr',
+                    'Marketing' => 'marketing@demo.fr',
+                ]
             ])
         ;
     }
